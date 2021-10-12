49eaa7870ca8fabc0146969e5da84b12cbd5e5e9
Jesper Nygaard Nielsen <jnn@laka.dk>
2021-10-12 09:58:15 +0200

    Initial sync

---
+++ JobmatchCRMSync/ModulusData/Kompetence.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JobmatchCRMSync.LAKAServices;
+using Oracle.ManagedDataAccess.Client;
+
+namespace JobmatchCRMSync.ModulusData
+{
+    class Kompetence
+    {
+        public Int32 IndividID { get; set; }
+        public Int32 FagID { get; set; }
+        public Boolean LinjefagJN { get; set; }
+        public Int32 ErfaringID { get; set; }
+
+        public static List<Kompetence> HentKompetencer()
+        {
+            List<Kompetence> KompList = new List<Kompetence>();
+            LAKAServicesSoapClient client = new LAKAServicesSoapClient();
+            OracleConnection con = new OracleConnection(client.GetDataSource("KUNDEDLFAPROD"));
+            
+            using (con)
+            {
+                string sql;
+                con.Open();
+                sql = "select * From v_jobmatch_CRM_SYNC";
+
+
+
+            }
+
+                
+            return KompList;
+        }
+    }
+}
--- JobmatchCRMSync/Program.cs
+++ JobmatchCRMSync/Program.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JobmatchCRMSync.LAKAServices;
+using System.ServiceModel.Description;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+
 
 namespace JobmatchCRMSync
 {
@@ -10,6 +16,16 @@ namespace JobmatchCRMSync
     {
         static void Main(string[] args)
         {
+            #region Forbinder til CRM
+            LAKAServicesSoapClient client = new LAKAServicesSoapClient();
+            Uri urlcrm = new Uri(client.CRMOrgService("PROD"));
+            ClientCredentials credentials = new ClientCredentials();
+            credentials.Windows.ClientCredential.UserName = client.CRMOrgUsername();
+            credentials.Windows.ClientCredential.Password = client.CRMOrgPassword();
+            OrganizationServiceProxy serviceproxy = new OrganizationServiceProxy(urlcrm, null, credentials, null);
+            IOrganizationService service;
+            service = (IOrganizationService)serviceproxy;
+            #endregion
         }
     }
 }
