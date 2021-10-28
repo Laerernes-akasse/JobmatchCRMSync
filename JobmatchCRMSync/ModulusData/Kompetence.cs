7a55ca8bdb7c1e2fb3c34e94a17fbcd1c970e0bd
Jesper Nygaard Nielsen <jnn@laka.dk>
2021-10-28 08:58:44 +0200

    Tilføjet kode så vi henter korrekt data fra CRM.

--- JobmatchCRMSync/ModulusData/Kompetence.cs
+++ JobmatchCRMSync/ModulusData/Kompetence.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using JobmatchCRMSync.LAKAServices;
 using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
 
 namespace JobmatchCRMSync.ModulusData
 {
@@ -12,26 +13,35 @@ namespace JobmatchCRMSync.ModulusData
     {
         public Int32 IndividID { get; set; }
         public Int32 FagID { get; set; }
-        public Boolean LinjefagJN { get; set; }
+        public Char LinjefagJN { get; set; }
         public Int32 ErfaringID { get; set; }
 
         public static List<Kompetence> HentKompetencer()
         {
             List<Kompetence> KompList = new List<Kompetence>();
             LAKAServicesSoapClient client = new LAKAServicesSoapClient();
-            OracleConnection con = new OracleConnection(client.GetDataSource("KUNDEDLFAPROD"));
-            
+            OracleConnection con = new OracleConnection(client.GetDataSource(ConfigurationManager.AppSettings["DataSource"].ToString()));
             using (con)
             {
                 string sql;
                 con.Open();
-                sql = "select * From v_jobmatch_CRM_SYNC";
+                sql = "select individ_id, erfaring, linjefagid, linjefag_jn From v_jobmatch_CRM_SYNC where rownum<1000";
+                //sql = "select 146258 as individ_id, 1 as erfaring, 18 as linjefagid, 'N' as linjefag_jn from dual";
+                OracleCommand cmd = new OracleCommand(sql, con);
+                OracleDataReader dr = cmd.ExecuteReader();
 
-
-
+                while (dr.Read())
+                {
+                    Kompetence komp = new Kompetence
+                    {
+                        IndividID = Convert.ToInt32(dr["individ_id"]),
+                        FagID = Convert.ToInt32(dr["linjefagid"]),
+                        ErfaringID = Convert.ToInt32(dr["erfaring"]),
+                        LinjefagJN = Convert.ToChar(dr["linjefag_jn"])
+                    };
+                    KompList.Add(komp);
+                }
             }
-
-                
             return KompList;
         }
     }
--- JobmatchCRMSync/Program.cs
+++ JobmatchCRMSync/Program.cs
@@ -7,8 +7,12 @@ using JobmatchCRMSync.LAKAServices;
 using System.ServiceModel.Description;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Client;
+using System.Configuration;
 using Microsoft.Xrm.Sdk.Query;
-
+using JobmatchCRMSync.ModulusData;
+using System.Collections.Specialized;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
 
 namespace JobmatchCRMSync
 {
@@ -18,7 +22,7 @@ namespace JobmatchCRMSync
         {
             #region Forbinder til CRM
             LAKAServicesSoapClient client = new LAKAServicesSoapClient();
-            Uri urlcrm = new Uri(client.CRMOrgService("PROD"));
+            Uri urlcrm = new Uri(client.CRMOrgService(ConfigurationManager.AppSettings["OrgService"].ToString()));
             ClientCredentials credentials = new ClientCredentials();
             credentials.Windows.ClientCredential.UserName = client.CRMOrgUsername();
             credentials.Windows.ClientCredential.Password = client.CRMOrgPassword();
@@ -26,6 +30,72 @@ namespace JobmatchCRMSync
             IOrganizationService service;
             service = (IOrganizationService)serviceproxy;
             #endregion
+
+            List<Kompetence> KompList = new List<Kompetence>();
+            KompList = ModulusData.Kompetence.HentKompetencer();
+
+
+            NameValueCollection ErfaringsTyper = new NameValueCollection()
+            {
+                { "1", "808260000" },
+                { "2", "808260001" },
+                { "3", "808260002" },
+                { "4", "808260003" },
+                { "5", "808260004" }
+            };
+
+            foreach (var komp in KompList)
+            {
+                //Vi finder det medlem som kompetencen er vedrørende
+                QueryByAttribute q_contact = new QueryByAttribute("contact");
+                q_contact.Attributes.Add("ak_modulusid"); //??
+                q_contact.Values.Add(komp.IndividID);
+                Entity _contact = service.RetrieveMultiple(q_contact).Entities.FirstOrDefault();
+                EntityReference ContactRef = new EntityReference("contact", _contact.Id); //TODO - relevant?
+
+                // Find korrekte fag udfra ID
+                QueryByAttribute qFag = new QueryByAttribute("lak_fag") //TODO tjek korrekte navne på attributter og entiteter
+                {
+                    ColumnSet = new ColumnSet(new string[] { "lak_name" })
+                };
+                qFag.Attributes.Add("lak_modulusid");
+                qFag.Values.Add(komp.FagID);
+                Entity EntFag = service.RetrieveMultiple(qFag).Entities.FirstOrDefault();
+                EntityReference FagRef = new EntityReference("lak_fag", EntFag.Id);
+
+                OptionSetValue osErfaring = new OptionSetValue(Convert.ToInt32(ErfaringsTyper[komp.ErfaringID]));
+
+                //Findes kompetencen i forvejen?
+                QueryByAttribute q_readKompetencer = new QueryByAttribute("lak_fagkompetence")
+                {
+                    ColumnSet = new ColumnSet(new string[] { "lak_fagkompetenceid" })
+                };
+                q_readKompetencer.Attributes.Add("lak_fag");
+                q_readKompetencer.Values.Add(FagRef.Id);
+                q_readKompetencer.Attributes.Add("lak_medlem");//????
+                q_readKompetencer.Values.Add(ContactRef.Id);
+
+                Entity Kompetence = new Entity("lak_fagkompetence");//TODO
+                // Har det pågældende medlem allerede denne kompetence registreret?
+                if (service.RetrieveMultiple(q_readKompetencer).Entities.Count() > 0)
+                {
+                    Console.WriteLine("Kompetencen findes i forvejen.");
+                    // Vi laver en update
+                    Kompetence.Attributes["lak_fagerfaring"] = osErfaring;//????
+                }
+                else
+                {
+                    Console.WriteLine("Vi opretter ny kompetence");
+                    //Ny kompetence
+                    //Entity Kompetence = new Entity("lak_fagkompetence");//TODO
+                    Kompetence.Attributes["lak_medlem"] = ContactRef;
+                    Kompetence.Attributes["lak_fag"] = FagRef;//????
+                    Kompetence.Attributes["lak_fagerfaring"] = osErfaring;//????
+                    Guid tempID = service.Create(Kompetence);
+                }
+                //Andre kompetencer
+
+            }
         }
     }
 }
